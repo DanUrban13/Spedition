98b24c449b126bf99e76ed1f8574d254ba1c7af4
urd33728 <daniel.urban@st.oth-regensburg.de>
2017-12-13 16:39:32 +0100

    implemented Entitymanager; implemented web Templateing and faces navData

--- src/main/java/de/urban/spedition/service/MitarbeiterService.java
+++ src/main/java/de/urban/spedition/service/MitarbeiterService.java
@@ -2,14 +2,22 @@ package de.urban.spedition.service;
 
 import de.urban.spedition.entity.Mitarbeiter;
 import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 @WebService
 public class MitarbeiterService implements MitarbeiterServiceIF {
+    
+    @PersistenceContext
+    private EntityManager em;
 
+    @Transactional
     @Override
     public Mitarbeiter erstelleMitarbeiter(Mitarbeiter neuerMitarbeiter) {
         System.out.println("Erstelle neuen Mitarbeiter: "
                 + neuerMitarbeiter.getVorname() + " " + neuerMitarbeiter.getName());
+        em.persist(neuerMitarbeiter);
         return null;
     }
     
